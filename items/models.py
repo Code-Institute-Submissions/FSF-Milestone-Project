5b50cc2ffcffe6cdb4d38b5a04f2e9d0d502e84b
PokeyMcFork <pokeymcfork@gmail.com>
2021-02-10 17:21:39 +0000

    added auction model, started checkout app

--- items/models.py
+++ items/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -19,15 +20,23 @@ class Item(models.Model):
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
     description = models.TextField()
     base_price = models.DecimalField(max_digits=10, decimal_places=2)
-    isAuction = models.BooleanField(default=False)
-    currentPrice = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    averageRating = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
+    is_auction = models.BooleanField(default=False)
+    average_rating = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
     # will need a recalc function
-    imageUrl = models.URLField(max_length=1024, blank=True)
+    image_URL = models.URLField(max_length=1024, blank=True)
     image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Auction(models.Model):
+    item = models.OneToOneField(Item, on_delete=models.CASCADE)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    last_bidder = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.item.name
 
 
 # sale model #
