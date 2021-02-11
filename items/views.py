1c564c638551ab593dfd824d988b977af3f28a3a
PokeyMcFork <pokeymcfork@gmail.com>
2021-02-11 03:04:16 +0000

    added item app paths

--- items/views.py
+++ items/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def search(request):
+    return render(request, 'items/itemlist.html')
+
+
+def item_page(request):
+    return render(request, 'items/itempage.html')
