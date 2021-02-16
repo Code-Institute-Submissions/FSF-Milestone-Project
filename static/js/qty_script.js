e6bc0340608a1d715641d8dbf8bcfc8938f954e7
PokeyMcFork <pokeymcfork@gmail.com>
2021-02-16 06:07:00 +0000

    some minor syntax fixes

--- static/js/qty_script.js
+++ static/js/qty_script.js
@@ -3,8 +3,8 @@ function handleEnableDisable(itemId){
     var currentValue = parseInt($(`#id_qty_${itemId}`).val());
     var minusDisabled = currentValue < 2;
     var plusDisabled = currentValue > 98;
-    $(`#decrement-qty_${itemId}`).prop('disabled', minusDisabled)
-    $(`#increment-qty_${itemId}`).prop('disabled', plusDisabled)
+    $(`#decrement-qty_${itemId}`).prop('disabled', minusDisabled);
+    $(`#increment-qty_${itemId}`).prop('disabled', plusDisabled);
 }
 
 var allQtyInputs = $('.qty_input');
@@ -22,7 +22,7 @@ $('.increment-qty').click(function(e){
     e.preventDefault();
     var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
     var currentValue = pareInt($(closestInput).val());
-    $(closestInput).val(currentValue - 1);
+    $(closestInput).val(currentValue + 1);
     var itemId = $(this).data('item_id');
     handleEnableDisable(itemId);
 });
@@ -31,7 +31,7 @@ $('.decrement-qty').click(function(e){
     e.preventDefault();
     var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
     var currentValue = pareInt($(closestInput).val());
-    $(closestInput).val(currentValue + 1);
+    $(closestInput).val(currentValue - 1);
     var itemId = $(this).data('item_id');
     handleEnableDisable(itemId);
 });
