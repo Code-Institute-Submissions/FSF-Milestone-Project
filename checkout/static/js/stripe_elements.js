c64f43506aebca6b3a218f51382f74d77c6dffcc
PokeyMcFork <pokeymcfork@gmail.com>
2021-02-21 23:27:47 +0000

    added checkout functionality awaiting acknowledge page & order creation

---
+++ checkout/static/js/stripe_elements.js
@@ -0,0 +1,50 @@
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1,-1);
+var client_secret = $('#id_client_secret').text().slice(1,-1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+
+//style var goes here
+
+var card = elements.create('card');
+card.mount('#card-element');
+
+
+card.addEventListener('change', function(event){
+    if (event.error){
+            $('#card-errors').html(`
+                <span class="icon" role="alert">
+                    <i class="fas fas-times"></i>
+                </span>
+                <span>${event.error.message}</span>`)}
+    else{
+        $('#card-errors').html('');
+    };
+});
+
+var form = document.getElementById('payment-form')
+
+form.addEventListener('submit', function(ev){
+    ev.preventDefault(); //stops default action, so script can do it's thing.
+    card.update({ 'disabled': true}); // disables card and submit button when clicked, to prevent multiple submissions.
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(client_secret,{
+        payment_method:{
+            card: card,
+        }
+    }).then(function(result){
+        if(result.error){
+            $('#card-errors').html(`
+                <span class="icon" role="alert">
+                    <i class="fas fas-times"></i>
+                </span>
+                <span>${event.error.message}</span>`)
+            card.update({'disabled':false});
+            $('#submit-button').attr('disabled', false);
+        }
+        else{
+            if (result.paymentIntent.status === 'succeeded'){
+                form.submit();
+            }
+        };
+    })
+})
--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,36 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 # Create your views here.
 
 from .forms import OrderForm
+from cart.context import cart_contents
+
+import stripe
 
 
 def checkout(request):
+
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment")
         return redirect(reverse('search'))
 
+    current_cart = cart_contents(request)
+    stripe_total = round(current_cart['grand_total'] * 100)
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+
     order_form = OrderForm()
     template = "checkout/checkout.html"
     context = {
         'order_form': order_form,
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+        'client_secret': intent.client_secret
     }
 
     return render(request, template, context)
