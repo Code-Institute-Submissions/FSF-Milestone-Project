f6f195a225782330777a924f0391c0169271ad39
PokeyMcFork <pokeymcfork@gmail.com>
2021-02-21 03:32:40 +0000

    page styling and index sales compatibility

--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from items.models import Item, Sale
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    most_viewed = Item.objects.all()
+    sales = Sale.objects.all()
+
+    context = {
+        'popular_items': most_viewed,
+        'sales': sales,
+    }
+
+    return render(request, 'home/index.html', context)
